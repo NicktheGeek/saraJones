b4e80359eae3a9fc78eaf765e048a7fb476215c0
Nick Croft <NicktheGeek@NickGeek.com>
2025-03-30 16:57:20 -0400

    add sitemap

---
+++ src/routes/sitemap.xml/+server.js
@@ -0,0 +1,44 @@
+export async function GET() {
+	return new Response(
+		`
+		<?xml version="1.0" encoding="UTF-8" ?>
+		<urlset
+			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+			xmlns:xhtml="https://www.w3.org/1999/xhtml"
+			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+		>
+			<url>
+				<loc>https://www.sarajonesforschoolboard.com/</loc>
+				<lastmod>2025-03-30</lastmod>
+				<changefreq>daily</changefreq>
+				<priority>1.0</priority>
+			</url>
+			<url>
+				<loc>https://www.sarajonesforschoolboard.com/statements</loc>
+				<lastmod>2025-03-30</lastmod>
+				<changefreq>daily</changefreq>
+				<priority>0.9</priority>
+			</url>
+			<url>
+				<loc>https://www.sarajonesforschoolboard.com/statements/announcement-of-candidacy</loc>
+				<lastmod>2025-03-30</lastmod>
+				<changefreq>daily</changefreq>
+				<priority>0.8</priority>
+			</url>
+			<url>
+				<loc>https://www.sarajonesforschoolboard.com/accessibility</loc>
+				<lastmod>2025-03-30</lastmod>
+				<changefreq>daily</changefreq>
+				<priority>0.8</priority>
+			</url>
+		</urlset>`.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		}
+	);
+}
