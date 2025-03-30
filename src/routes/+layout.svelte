038e0a18034f115af2e8821be6162443bc3b3e26
Nick Croft <NicktheGeek@NickGeek.com>
2025-03-30 16:24:19 -0400

    import styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
     import Header from '$lib/components/header/Header.svelte';
     import Footer from '$lib/components/footer/Footer.svelte';
+    import '../assets/styles/reset.css';
+    import '../assets/styles/global.css';
 </script>
 <style>
     main {
