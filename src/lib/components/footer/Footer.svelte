56b9f2c53b5cdd2525caf685a4e7c7a3687292d4
Nick Croft <NicktheGeek@NickGeek.com>
2025-03-30 14:35:13 -0400

    add accessibility statement

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -1,23 +1,87 @@
 <script>
-
 	import Logo from "../header/Logo.svelte";
 	import Nav from "./Nav.svelte";
 	import Vote from "./Vote.svelte";
 
+	$effect(() => {
+		const year = new Date().getFullYear();
+		const copyrightYear = document.getElementById("copyright-year");
+
+		if (copyrightYear) {
+			copyrightYear.textContent = year.toString();
+		}
+	});
 </script>
 <footer class="wrapper">
-    <div class="inner">
-        <Logo />
-        <Nav />
-        <Vote />
-    </div>
+	<div class="inner">
+		<Logo />
+		<Nav />
+		<Vote />
+	</div>
+	<div class="copyright inner">
+		<p>©<span id="copyright-year">2025</span> Sara Jones for School Board</p>
+		<p>Website by <a href="https://designsbynickthegeek.com">Nick Croft</a></p>
+		<nav aria-label="Additional Information">
+			<ul class="nav">
+				<li>
+					<a href="/accessibility">Accessibility</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
 </footer>
 <style>
-    .wrapper {
-        --inner-flex-direction: row;
-        --inner-flex-wrap: wrap;
-        --inner-flex-justify: space-between;
-        background: var(--bg);
-        padding: 1rem var(--content-padding);
-    }
+	.wrapper {
+		--inner-flex-direction: row;
+		--inner-flex-wrap: wrap;
+		--inner-flex-justify: space-between;
+		background: var(--bg);
+		padding: 1rem var(--content-padding);
+		flex-direction: column;
+	}
+
+	.copyright {
+		display: flex;
+		justify-content: left;
+		align-items: center;
+		flex-wrap: wrap;
+		column-gap: 1rem;
+		row-gap: 0.5rem;
+
+		p {
+			margin: 0;
+			font-size: 1rem;
+			color: var(--text);
+		}
+
+		a {
+				color: var(--text);
+				text-decoration: underline;
+
+				&:hover,
+				&:focus {
+					text-decoration: none;
+				}
+
+				&:visited {
+					color: var(--text);
+				}
+			}
+
+		nav {
+			margin-left: auto;
+		}
+
+		.nav {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			row-gap: 1rem;
+			column-gap: 0.5rem;
+			flex-wrap: wrap;
+			margin: 0;
+			padding: 0;
+			list-style: none;
+		}
+	}
 </style>
---
+++ src/routes/accessibility/+page.svelte
@@ -0,0 +1,63 @@
+<section class="wrapper">
+	<div class="inner">
+		<h1>Accessibility Statement for <span class="basic-information website-name">Sara Jones for School Board</span></h1>
+		<div class="content">
+			<h3>Conformance status</h3>
+			<p>
+				The <a href="https://www.w3.org/WAI/standards-guidelines/wcag/">Web Content Accessibility Guidelines (WCAG)</a> defines requirements for designers and developers to improve accessibility for people with disabilities. It defines three levels of conformance: Level A, Level AA, and Level AAA.
+				<span class="basic-information website-name">Sara Jones for School Board</span>
+				is
+				<span class="basic-information conformance-status" data-printfilter="lowercase">fully conformant</span>
+				with
+				<span class="basic-information conformance-standard"><span data-negate="">WCAG 2.2 level AA</span>.</span>
+				<span>
+				<span class="basic-information conformance-status">Fully conformant</span>
+				means that
+				<span class="basic-information conformance-meaning">the content fully conforms to the accessibility standard without any exceptions</span>.
+			</span>
+			</p>
+			<h3>Technical specifications</h3>
+			<p>
+				Accessibility of
+				<span class="basic-information website-name">Sara Jones for School Board</span>
+				relies on the following technologies to work with the particular combination of web browser and any assistive technologies or plugins installed on your computer:
+			</p>
+			<ul class="technical-information technologies-used">
+				<li>HTML</li>
+				<li>WAI-ARIA</li>
+				<li>CSS</li>
+				<li>JavaScript</li>
+			</ul>
+			<p>These technologies are relied upon for conformance with the accessibility standards used.</p>
+			<h3>Assessment approach</h3>
+			<p>
+				<span class="basic-information organization-name">Sara Jones for School Board</span>
+				assessed the accessibility of
+				<span class="basic-information website-name">Sara Jones for School Board</span>
+				by the following approaches:
+			</p>
+			<ul class="technical-information assessment-approaches">
+				<li>Self-evaluation: Developer has WAS from IAAP in good standing.</li>
+			</ul>
+			<hr noshade="noshade">
+			<h3>Date</h3>
+			<p>
+				This statement was created on
+				<span class="basic-information statement-created-date">30 March 2025</span>
+				using the <a href="https://www.w3.org/WAI/planning/statements/">W3C Accessibility Statement Generator Tool</a>.
+			</p>
+		</div>
+	</div>
+</section>
+<style>
+	.wrapper {
+		--content-width: 800px;
+	}
+
+	h1 {
+		background-color: var(--bg);
+		padding: 1rem;
+		text-align: center;
+		width: 100%;
+	}
+</style>
