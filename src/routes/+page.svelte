af8e63c0dbf94e68209ab5e7df37979c40dd3a04
Nick Croft <NicktheGeek@NickGeek.com>
2025-03-30 12:39:42 -0400

    add title and meta to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,11 @@
     import Hero from "$lib/components/home/Hero.svelte";
 </script>
 
+<svelte:head>
+	<title>Sara Jones for Warren County VA School Board | Home</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={`Sara Jones for Warren County VA School Board | Home`} />
+</svelte:head>
+
 <Hero />
 <Content />
